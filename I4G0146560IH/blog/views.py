f595cfc42c5048cedc8c5fc69a99430203fac11a
Oputa Olivia Amarachi <72948572+oputaolivia@users.noreply.github.com>
2022-06-25 22:11:25 +0000

    First Commit

---
+++ I4G0146560IH/blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    template_name = 'base.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy("blog:all")
+    template_name = 'post_form.html'
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy("blog:all")
+    template_name = 'post_confirm_delete.html'
